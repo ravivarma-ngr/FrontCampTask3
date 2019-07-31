8377bca4ade76e171152183f037de9a42888116b
ravivarma-ngr <ravivarma.ngr@gmail.com>
2019-07-31 14:58:26 +0530

    Add files via upload

---
+++ Endpoints.js
@@ -0,0 +1,5 @@
+export const newsApiEndPoint = {
+  getAllChannelsEndpoint: "https://newsapi.org/v1/sources",
+  key: "b00e28a8dcd14312834244c42f873032",
+  articlesEndPoint: "https://newsapi.org/v1/articles?"
+};
